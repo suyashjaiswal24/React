45f3cf25e3bacdade608113edf27fcb518f8a083
Suyash Jaiswal <suyash.jaiswal@navex.com>
2026-06-28 23:16:56 +0530

    On demand adding more counters

--- React_Refresher_Piyush_Cohort/React-TS-Refresher/src/App.tsx
+++ React_Refresher_Piyush_Cohort/React-TS-Refresher/src/App.tsx
@@ -1,22 +1,26 @@
+import { useState } from 'react';
 import './App.css'
 import Counter from './components/Counter';
-import { useRandomUser } from './hooks/user-random'
+// import { useRandomUser } from './hooks/user-random'
 
 function App() {
 
-    const { user, fetchRandomUser, isFetching, error } = useRandomUser();
+    // const { user, fetchRandomUser, isFetching, error } = useRandomUser();
+    const [count, setCount] = useState(1);
     return (
         <>
-            <h1>Hello ji</h1>
+            {/* <h1>Hello ji</h1>
             <button onClick={fetchRandomUser}>Fetch Random User</button>
             {error && <p style={{ color: 'red' }}>{error}</p>}
             {user ? (
                 isFetching ? (
                     <p>Loading...</p>
                 ) : (<h1>{user.name.first} {user.name.last}</h1>)
-            ) : "No User Fetched Yet"}
+            ) : "No User Fetched Yet"} */}
 
-            {new Array(10).fill(null).map(() => <Counter />)}  {/*Render 10 Counter components using map */}
+            <button onClick={() => setCount(count + 1)}>Add Counter</button>
+
+            {new Array(count).fill(null).map(() => <Counter />)}  {/*Render 10 Counter components using map */}
         </>
     )
 }
